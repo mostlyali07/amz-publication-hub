32b9a1d970a3098281d07a67e3842213cad277d8
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-05-30 19:15:45 +0500

    Update

--- e-book-writing-publishing-service.php
+++ e-book-writing-publishing-service.php
@@ -14,7 +14,7 @@
                 <div class="col-md-6">
                     <h1 style="margin: 150px 0px 0px 0px" class="mb-3"><strong>E-Book Writing & Publishing</strong></h1>
                     <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate provident nesciunt commodi sit quo inventore quis, dolores ipsam dicta praesentium aut, accusantium, porro libero. Error porro amet optio quod ut!</p>
-                    <a class="btn-1" href="#">BOOK PUBLISHING</a> &nbsp;&nbsp;
+                    <a class="btn-1" href="#">LET'S STARTED</a> &nbsp;&nbsp;
                     <a class="btn-1" href="#">BOOK MARKETING</a>
                     <?php include './Includes/main-form.php'; ?>
 
@@ -25,6 +25,35 @@
         </div>
     </div>
 
+    <!-------- Section Start -------->
+    <div class="container py-5 animate">
+        <div class="row">
+            <div class="col-md-6 text-center">
+                <img src="./Images/service-img-2.jpg" class="sec-img" />
+            </div>
+            <div class="col-md-6 d-flex align-items-center right-sec">
+                <div>
+                    <h2 class="fw-bold">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    </h2>
+                    <div class="divider"></div>
+                    <p class="mb-4">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias illo eum
+                        unde, perspiciatis, culpa iste sapiente odio minima ad neque saepe cupiditate,
+                        atque debitis obcaecati laboriosam vitae! Ea, itaque provident? <br>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias illo eum
+                        unde, perspiciatis, culpa iste sapiente odio minima ad neque saepe cupiditate,
+                        atque debitis obcaecati laboriosam vitae! Ea, itaque provident?
+                    </p>
+                    <a href="#" class="btn-1">GET A QUOTE</a> &nbsp;&nbsp;
+                    <a href="#" class="btn-1">LEARN MORE</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-------- Section End -------->
+
+    <?php include './Includes/faqs.php'; ?>
     <?php include './Includes/footer.php'; ?>
 </body>
 
--- index.php
+++ index.php
@@ -183,147 +183,12 @@
 
 
 
-  <!-------- Seventh Section Start -------->
-  <div class="container py-5 animate">
-    <div class="row">
-      <div class="col-md-12">
-        <h2 class="fw-bold text-center mb-4">Our Work</h2>
-        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel" data-bs-touch="true">
-          <div class="carousel-inner py-3">
-            <div class="carousel-item active">
-              <div class="row">
-                <div class="col-md-4 mac d-flex align-items-center justify-content-center">
-                  <img src="./Images/mac-book1.png" />
-                </div>
-                <div class="col-md-3 bok d-flex align-items-center justify-content-center">
-                  <img src="./Images/francis-book.jpg" />
-                </div>
-                <div class="col-md-5 d-flex align-items-center">
-                  <div class="author-rev">
-                    <p>
-                      I’m very grateful to the Amazon Publishing Hub team;
-                      Antonio, Walter, and the other team members who worked
-                      on my book are excellent. This team at Amazon Publish
-                      does outstanding work. They’ve been accommodating in
-                      getting my first book published on time. The editing
-                      team is fantastic at catching some important typos etc.,
-                      and the marketing team, too, is outstanding. I had
-                      excellent communication and was updated weekly at every
-                      stage of my first book project. I’m currently working
-                      with the same team on my second book and possibly the
-                      third soon. Keep up the excellent work you are doing.
-                      Love you all.
-                    </p>
-                    <div class="author d-flex align-items-center">
-                      <h6 class="fw-bold">
-                        Francis Dakubo <br />
-                        <span> Excellent work by Amazon Publishing Hub </span>
-                      </h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item py-3">
-              <div class="row">
-                <div class="col-md-6 bok d-flex align-items-center justify-content-center">
-                  <img src="./Images/Raeneesa-book.png" style="width: 400px" />
-                </div>
-                <div class="col-md-6 d-flex align-items-center">
-                  <div class="author-rev">
-                    <p>
-                      My experience has been wonderful! I started working with
-                      the team in the new year and by the end of February, I
-                      not only had written the book but it was edited and
-                      published. Ted and the rest of the team have been very
-                      nice! They work super hard and are very good at what
-                      they do. Most importantly they have been easy and stress
-                      free to work with. Not once did I feel as if I was
-                      pressured to turn in my manuscript. Not once did they
-                      make me feel bad about my writing or any choices that I
-                      mad. They let me have as much control over my work that
-                      I wanted. I have enjoyed working this them very much and
-                      I hope to continue this relationship. 10/10 would
-                      recommend this service to friends.
-                    </p>
-                    <div class="author d-flex align-items-center">
-                      <h6 class="fw-bold">
-                        Raeneesa N. Bey-Lockwood <sub>(Daffodil Rae)</sub>
-                        <br />
-                        <span> My experience has been wonderful </span>
-                      </h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item py-3">
-              <div class="row">
-                <div class="col-md-6 bok d-flex align-items-center justify-content-center">
-                  <img src="./Images/Kathleen-book.png" style="width: 380px" />
-                </div>
-                <div class="col-md-6 d-flex align-items-center">
-                  <div class="author-rev">
-                    <p>
-                      AMAZON Publishing Hub is a great way to publish your
-                      book. My experience with Ted and his team was terrific!
-                      Communication played a big key in the process. Their
-                      professionalism and knowledge helped make my dream a
-                      reality. It was a short process to get my children’s
-                      book published. The Press Release was so wonderfully
-                      awesome as well! I’m very touched beyond words. My
-                      thanks and appreciation to Ted and his team! My heart is
-                      still smiling!
-                    </p>
-                    <div class="author d-flex align-items-center">
-                      <h6 class="fw-bold">
-                        Kathleen Beirne <br />
-                        <span>AMAZON Publishing Hub is a great way to…</span>
-                      </h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="carousel-item py-3">
-              <div class="row">
-                <div class="col-md-6 bok d-flex align-items-center justify-content-center">
-                  <img src="./Images/Keith-book.jpg" style="width: 380px" />
-                </div>
-                <div class="col-md-6 d-flex align-items-center">
-                  <div class="author-rev">
-                    <p>
-                      Zara Morton was incredible to work with in the
-                      publishing of my book. Response time was quick and
-                      answers were complete. I highly recommend!
-                    </p>
-                    <div class="author d-flex align-items-center">
-                      <h6 class="fw-bold">
-                        Keith Haygood <br />
-                        <span>Zara Morton was incredible to work with…</span>
-                      </h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <!-------- Section Start -------->
+  <?php include './Includes/reviews.php'; ?>
 
-  <!-------- Seventh Section End -------->
+  <!-------- Section End -------->
 
-  <!-------- Fifth Section Start -------->
+  <!-------- Section Start -------->
   <div class="compre">
     <div class="container py-5 animate">
       <div class="row">
@@ -419,7 +284,7 @@
       </div>
     </div>
   </div>
-  <!-------- Fifth Section End -------->
+  <!-------- Section End -------->
 
   <!-------- Sixth Section Start -------->
   <div class="container animate py-5 Choose">
