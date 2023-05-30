1355983af0a0968d7975fe984e90ca6f55873e4f
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-05-30 17:33:48 +0500

    Update and Create Inner pages

--- Includes/header.php
+++ Includes/header.php
@@ -1,86 +1,87 @@
  <!-------- Header Start -------->
  <div class="Topbar">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="d-flex justify-content-between">
-            <a href="tel:+18447270033"> Call Now +1-844-727-0033 </a>
-            <a href="mailto:marketing@amzpublicationhub.com">
-              Email : marketing@amzpublicationhub.com
-            </a>
-          </div>
-        </div>
-        <div class="col-md-6 text-end">
-          <a class="social" href="https://www.instagram.com/amzpublishinghub/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
-          <a class="social" href="https://www.facebook.com/amazonpublishinghub/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <header>
-    <nav class="navbar navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" href="/">
-          <img src="./Images/Black-Logo.png" width="250" />
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="navbarSupportedContent" class="collapse navbar-collapse">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 my-menu">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">HOME</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                SERVICES
-              </a>
+   <div class="container">
+     <div class="row">
+       <div class="col-md-6">
+         <div class="d-flex justify-content-between">
+           <a href="tel:+18447270033"> Call Now +1-844-727-0033 </a>
+           <a href="mailto:marketing@amzpublicationhub.com">
+             Email : marketing@amzpublicationhub.com
+           </a>
+         </div>
+       </div>
+       <div class="col-md-6 text-end">
+         <a class="social" href="https://www.instagram.com/amzpublishinghub/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+         <a class="social" href="https://www.facebook.com/amazonpublishinghub/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+       </div>
+     </div>
+   </div>
+ </div>
 
-              <ul class="dropdown-menu">
-                <li>
-                  <a class="dropdown-item" href="/">E-Book Writing & Publishing</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/">Book Editing & Proofreading</a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Book Cover Design </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Book Illustrations </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Audiobook Narration </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Book Publishing </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Book Printing </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Book Marketing </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Author Website </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="/"> Amazon-publishing </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/contact-us">BLOGS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/contact-us">OUR WORK </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about-us">CONTACT US</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-  <!-------- Header End -------->
+ <header>
+   <nav class="navbar navbar-expand-lg">
+     <div class="container">
+       <a class="navbar-brand" href="/">
+         <img src="./Images/Black-Logo.png" width="250" />
+       </a>
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div id="navbarSupportedContent" class="collapse navbar-collapse">
+         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+           <li class="nav-item">
+             <a class="nav-link my-menu" aria-current="page" href="/">HOME</a>
+           </li>
+           <li class="nav-item dropdown">
+             <a class="nav-link dropdown-toggle my-menu" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+               SERVICES
+             </a>
+
+             <ul class="dropdown-menu">
+               <li>
+                 <a class="dropdown-item" href="/">E-Book Writing & Publishing</a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/">Book Editing & Proofreading</a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Book Cover Design </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Book Illustrations </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Audiobook Narration </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Book Publishing </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Book Printing </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Book Marketing </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Author Website </a>
+               </li>
+               <li>
+                 <a class="dropdown-item" href="/"> Amazon-publishing </a>
+               </li>
+             </ul>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link my-menu" href="/contact-us">BLOGS</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link my-menu" href="/contact-us">OUR WORK </a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link my-menu" href="/about-us">CONTACT US</a>
+           </li>
+         </ul>
+       </div>
+     </div>
+   </nav>
+ </header>
+ <!-------- Header End -------->
