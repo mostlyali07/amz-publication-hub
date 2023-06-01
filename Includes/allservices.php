2ea7aba5cd9b920a860e0058ae70e36c762f801a
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-06-01 19:22:51 +0500

    update

---
+++ Includes/allservices.php
@@ -0,0 +1,95 @@
+<div class="compre">
+    <div class="container py-5 animate">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h2 class="fw-bold mb-4">Our Comprehensive Services Include</h2>
+          <div class="d-flex flex-wrap justify-content-center">
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/online-learning.png" />
+                <a href=""> Amazon Book Publishing </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/editorial-design.png" />
+                <a href=""> Book Editing </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/5381226.png" />
+                <a href=""> Book Marketing </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/contract.png" />
+                <a href=""> Ghostwriting </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/storage.png" />
+                <a href=""> Book Publishing & Distribution </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/cover.png" />
+                <a href=""> Book Cover Design </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/1415359.png" />
+                <a href=""> Custom Illustration Design Services </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/1024321.png" />
+                <a href=""> Cover Book Illustration </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/book.png" />
+                <a href=""> Book Illustration </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/2471125.png" />
+                <a href=""> Childrens Book Illustration </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/film-editing.png" />
+                <a href=""> Cultural Accuracy Reading </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/feasibility.png" />
+                <a href=""> Author Book Promotion </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card1">
+              <div>
+                <img src="./Images/1197460.png" />
+                <a href=""> Author Website & Promotion </a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center card2">
+              <div>
+                <img src="./Images/2065161.png" />
+                <a href=""> Beta Reader Review </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
--- blogs.php
+++ blogs.php
@@ -9,7 +9,63 @@
 
 <body>
     <?php include './Includes/header.php'; ?>
+    <div class="blog-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div style="margin: 120px 0px 0px 0px">
+                        <h1>
+                            Our
+                            <span class="fw-bold"> Blogs</span>
+                        </h1>
+                        <p class="pb-3">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis tempora quasi velit accusantium aperiam ad deserunt
+                            temporibus nostrum aspernatur eum, natus id at nisi consequuntur.
+                            Aspernatur voluptatum alias atque voluptatem.
+                        </p>
+                        <a class="btn-1" href="#">CONTACT US</a>
+                    </div>
+                </div>
+                <div class="col-md-6"></div>
+            </div>
+        </div>
+    </div>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./Images/our-work.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./Images/our-work.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="./Images/our-work.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <?php include './Includes/faqs.php'; ?>
 
     <?php include './Includes/footer.php'; ?>
 </body>
